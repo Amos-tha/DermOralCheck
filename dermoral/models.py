fa0b6c7660318f1f104ad333dfabc5e2ec52b1bf
JingEn012 <113515442+JingEn012@users.noreply.github.com>
2023-11-06 19:45:47 +0800

    database

--- dermoral/models.py
+++ dermoral/models.py
@@ -3,22 +3,39 @@ from django.db import models
 # Create your models here.
 class Account(models.Model):
     phoneNo = models.CharField(primary_key=True, unique=True, max_length=20)
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200, null=True)
-    address = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    healthCondition = models.CharField(max_length=200, blank=True)
+    name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250, null=True)
+    address = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    healthCondition = models.CharField(max_length=250, blank=True)
 
-class SkinDisease(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    symptom = models.CharField(max_length=200)
-    cause = models.CharField(max_length=200)
+class Disease(models.Model):
+    # default auto increment if no put this line of code
+    # id = models.AutoField(primary_key=True) 
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    symptom = models.CharField(max_length=250)
+    cause = models.CharField(max_length=250)
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    sideEffect = models.CharField(max_length=250)
 
 class Record(models.Model):
     patient = models.ForeignKey(Account, on_delete=models.CASCADE,)
-    disease = models.ForeignKey(SkinDisease, on_delete=models.CASCADE,)
-    disease_img = models.ImageField(upload_to='')
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE,)
+    recordDate = models.DateField(auto_now=True)
+    recordTime = models.TimeField(auto_now=True)
+    disease_img = models.ImageField(upload_to='static/media')
+
+class Prescription(models.Model):
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE,)
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE,)
+
+
+
+
 
 
 
