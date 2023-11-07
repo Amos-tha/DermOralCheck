91f23afe0a51f433dd2793ff7fd15b02697fa583
JingEn012 <113515442+JingEn012@users.noreply.github.com>
2023-11-07 17:30:00 +0800

    dk

--- dermoral/views.py
+++ dermoral/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
 
 # Create your views here.
 def home(request):
+    tables = db_table_exists()
+    return HttpResponse(tables)
     return render(request, "map.html", {})
+
+def db_table_exists():
+    tables = connection.introspection.table_names()
+    return tables 
