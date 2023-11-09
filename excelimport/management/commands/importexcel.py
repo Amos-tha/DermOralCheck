907f833d1f69e5498f4f1fe5bb2d6d271d2c90a1
JingEn012 <113515442+JingEn012@users.noreply.github.com>
2023-11-09 17:54:35 +0800

    create a new folder for insert data to model from excel

---
+++ excelimport/management/commands/importexcel.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+import pandas as pd
+from django.db import connection
+from dermoral.models import Disease  # Replace with your actual model
+
+class Command(BaseCommand):
+    help = 'Import data from Excel into Django model'
+
+    def handle(self, *args, **options):
+        Disease.objects.all().delete()
+        
+        # Reset the primary key sequence for SQLite
+        with connection.cursor() as cursor:
+            cursor.execute(f"DELETE FROM sqlite_sequence WHERE name='{Disease._meta.db_table}';")
+
+        file_path = 'C:/Users/ASUS/Desktop/SkinDisease.xlsx'  # Replace with the actual file path
+
+        # Read Excel file into a DataFrame
+        df = pd.read_excel(file_path)
+
+        # Iterate through rows and create model instances
+        for index, row in df.iterrows():
+            instance = Disease(
+                name=row['Name'],  # Replace with your column names
+                description=row['Description'],
+                symptom=row['Symptom'],
+                cause=row['Cause'],
+                # Add other fields as needed
+            )
+            instance.save()
+
+        self.stdout.write(self.style.SUCCESS('Data imported successfully.'))
+    
