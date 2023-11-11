c60d8b28bb24ad070b0f4dfdd6f1ca0ad38f8dcf
JingEn012 <113515442+JingEn012@users.noreply.github.com>
2023-11-12 01:15:38 +0800

    mariadb

--- derma_oral_check/settings.py
+++ derma_oral_check/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'derma_oral_check.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fyp',
+        'USER': 'admin',
+        'PASSWORD': 'abcd1234',
+        'HOST': 'fyp.caldwcg5mkux.us-east-1.rds.amazonaws.com',
+        'POST': '3306',
     }
 }
 
